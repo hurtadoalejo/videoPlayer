3b119145541e084a5f8d4beb6d22e8c44ab6b901
hurtadoalejo <alejohg2911@gmail.com>
2025-04-24 16:48:00 -0500

    Cambio clase facade

--- co.edu.uniquindio.reproductor/src/main/java/co/edu/uniquindio/reproductor/reproductorapp/facade/VideoFacade.java
+++ co.edu.uniquindio.reproductor/src/main/java/co/edu/uniquindio/reproductor/reproductorapp/facade/VideoFacade.java
@@ -6,16 +6,8 @@ import co.edu.uniquindio.reproductor.reproductorapp.model.VideoFile;
 import co.edu.uniquindio.reproductor.reproductorapp.model.VideoPlayer;
 
 public class VideoFacade {
-    private Codec codec;
-    private VideoDecoder videoDecoder;
-    private VideoFile videoFile;
-    private VideoPlayer videoPlayer;
 
     public VideoFacade() {
-        codec = new Codec();
-       videoDecoder = new VideoDecoder();
-       videoFile = new VideoFile();
-       videoPlayer = new VideoPlayer();
     }
 
     public String reproducir(String fileName, String type) {
@@ -25,6 +17,5 @@ public class VideoFacade {
         VideoPlayer videoPlayer = new VideoPlayer();
         return videoFile.load(fileName) + "\n" + codec.codec(type) + "\n" +
                 videoDecoder.decode(fileName, type) + "\n" + videoPlayer.play(fileName);
-
     }
 }
